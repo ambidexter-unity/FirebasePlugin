c09b5924369f2faa1c5783e5199633fafe09b977
aaamarkin <aaamarkin@gmail.com>
2019-10-20 15:17:56 +0300

    Update in gcloud error logging

--- Editor/gcloud_run.bash
+++ Editor/gcloud_run.bash
@@ -3,5 +3,4 @@ echo "Starting to deploy... Please wait... "
 cd ./Assets/Server~
 $1 builds submit --tag gcr.io/coinseast/server
 $1 beta run deploy --image gcr.io/coinseast/server --platform managed --region europe-west1 --allow-unauthenticated server
-echo "Deployment complete"
 exit 0
