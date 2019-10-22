b25a750420916323905c9925733d314c78b52091
GeorgAmisare <orlegand@gmail.com>
2019-10-22 16:55:21 +0300

    Add UNITY_EDITOR preprocessors

--- Assets/Plugins/FirebasePlugin/Editor/ShellHelper.cs
+++ Assets/Plugins/FirebasePlugin/Editor/ShellHelper.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿#if UNITY_EDITOR
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.Diagnostics;
@@ -302,3 +303,4 @@ public class ShellHelper
 		return ShellHelper.ProcessCommand(cmd, workDir, _enviroumentVars);
 	}
 }
+#endif
