9ca51ba15debd648af7927a4944d27be5f408cd0
aaamarkin <aaamarkin@gmail.com>
2019-11-05 23:52:19 +0300

    Modification in creating local server

--- Assets/Plugins/FirebasePlugin/Editor/local_run.bash
+++ Assets/Plugins/FirebasePlugin/Editor/local_run.bash
@@ -4,7 +4,8 @@ cd ./Assets/Server~
 export PATH=/usr/local/bin:/Library/Apple/usr/bin:/Library/Apple/bin:/usr/local/go/bin:$PATH
 source ~/.bash_profile
 command -v docker >/dev/null 2>&1 || { echo >&2 "Docker not installed. Please install docker."; exit 1; }
-docker build . --tag gcr.io/coinseast/server
+docker build . --tag gcr.io/coinseast/server \
+  --cache-from gcr.io/coinseast/server:latest
 $1 auth configure-docker
 PORT=8080 && docker run \
    -p 8080:${PORT} \
@@ -12,6 +13,4 @@ PORT=8080 && docker run \
    -e K_SERVICE=dev \
    -e K_CONFIGURATION=dev \
    -e K_REVISION=dev-00001 \
-   -e GOOGLE_APPLICATION_CREDENTIALS=/tmp/keys/credentials.json \
-   -v $GOOGLE_APPLICATION_CREDENTIALS:/tmp/keys/credentials.json:ro \
    gcr.io/coinseast/server
