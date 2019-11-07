a1d4d28cb55e09c01dc6a0f07dac5a4c04fe192c
aaamarkin <aaamarkin@gmail.com>
2019-11-07 19:06:39 +0300

    Local and Cloud docker run modifies server config properties

--- Editor/local_run.bash
+++ Editor/local_run.bash
@@ -4,8 +4,8 @@ cd ./Assets/Server~
 export PATH=/usr/local/bin:/Library/Apple/usr/bin:/Library/Apple/bin:/usr/local/go/bin:$PATH
 source ~/.bash_profile
 command -v docker >/dev/null 2>&1 || { echo >&2 "Docker not installed. Please install docker."; exit 1; }
-docker build . --tag gcr.io/coinseast/server \
-  --cache-from gcr.io/coinseast/server:latest
+docker build . --tag gcr.io/$2/server \
+  --cache-from gcr.io/$2/server:latest
 $1 auth configure-docker
 PORT=8080 && docker run \
    -p 8080:${PORT} \
@@ -13,4 +13,4 @@ PORT=8080 && docker run \
    -e K_SERVICE=dev \
    -e K_CONFIGURATION=dev \
    -e K_REVISION=dev-00001 \
-   gcr.io/coinseast/server
+   gcr.io/$2/server
